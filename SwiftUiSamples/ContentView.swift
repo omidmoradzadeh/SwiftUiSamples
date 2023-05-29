316f574b5c14c452fe02dc48439a585505eb5b9f
omidmoradzadeh <36116834+omidmoradzadeh@users.noreply.github.com>
2023-05-29 10:21:41 +0300

    Initial Commit

---
+++ SwiftUiSamples/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUiSamples
+//
+//  Created by Omid on 29.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUiSamples/SwiftUiSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiSamplesApp.swift
+//  SwiftUiSamples
+//
+//  Created by Omid on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
